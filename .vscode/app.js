bd0fba055afae8ed9b33ec7e64311f3548c7b9d0
cedricrock03 <153559682+cedricrock03@users.noreply.github.com>
2024-02-06 05:44:46 +0000

    initial commit push code to repo

---
+++ .vscode/app.js
@@ -0,0 +1,17 @@
+let row = ''
+for (let a = 1; a <= 3; a++) {
+    for (let b = 1; b <= 3; b++) {
+    row += '1.'
+}
+console.log(row)
+row = ''
+}
+
+const firstName = ['Cedric', 'James', 'Jack',]
+const lastName = ['Rock', 'Johnson', 'Williams']
+
+for (let a = 0; a < firstName.length; a++) {
+    for (let b = 0; b < lastName.length; b++ )
+
+    console.log(students[i], lastName[j])
+}
